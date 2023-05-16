ddad954d3e27c4349034c50f762af66f36c4ca90
FlySwitDesigner <111734834+FlySwitDesigner@users.noreply.github.com>
2023-05-16 01:26:08 -0500

    Update taller.cpp

--- taller.cpp
+++ taller.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-// Estructura de un nodo del árbol AVL
+
 struct Nodo {
     string nombre;
     int numero;
@@ -13,7 +13,7 @@ struct Nodo {
     int altura;
 };
 
-// Función para obtener la altura de un nodo
+
 int altura(Nodo* nodo) {
     if (nodo == NULL) {
         return 0;
@@ -21,12 +21,12 @@ int altura(Nodo* nodo) {
     return nodo->altura;
 }
 
-// Función para obtener el máximo de dos números
+
 int maximo(int a, int b) {
     return (a > b) ? a : b;
 }
 
-// Función para crear un nuevo nodo
+
 Nodo* nuevoNodo(string nombre, int numero, int vueltas, double tiempo_total) {
     Nodo* nodo = new Nodo();
     nodo->nombre = nombre;
@@ -39,7 +39,7 @@ Nodo* nuevoNodo(string nombre, int numero, int vueltas, double tiempo_total) {
     return nodo;
 }
 
-// Función para rotar a la derecha un subárbol
+
 Nodo* rotarDerecha(Nodo* nodo) {
     Nodo* nodo_izquierda = nodo->izquierda;
     Nodo* nodo_derecha = nodo_izquierda->derecha;
@@ -53,7 +53,7 @@ Nodo* rotarDerecha(Nodo* nodo) {
     return nodo_izquierda;
 }
 
-// Función para rotar a la izquierda un subárbol
+
 Nodo* rotarIzquierda(Nodo* nodo) {
     Nodo* nodo_derecha = nodo->derecha;
     Nodo* nodo_izquierda = nodo_derecha->izquierda;
@@ -67,7 +67,7 @@ Nodo* rotarIzquierda(Nodo* nodo) {
     return nodo_derecha;
 }
 
-// Función para obtener la diferencia de altura entre los subárboles izquierdo y derecho de un nodo
+
 int balance(Nodo* nodo) {
     if (nodo == NULL) {
         return 0;
@@ -75,35 +75,35 @@ int balance(Nodo* nodo) {
     return altura(nodo->izquierda) - altura(nodo->derecha);
 }
 
-// Función para insertar un nuevo nodo en el árbol AVL
+
 Nodo* insertar(Nodo* nodo, string nombre, int numero, int vueltas, double tiempo_total) {
-    // Si el árbol está vacío, se crea un nuevo nodo
+
     if (nodo == NULL) {
         return nuevoNodo(nombre, numero, vueltas, tiempo_total);
     }
 
-    // Se inserta el nodo en el subárbol izquierdo
+
     if (tiempo_total < nodo->tiempo_total) {
         nodo->izquierda = insertar(nodo->izquierda, nombre, numero, vueltas, tiempo_total);
     }
 
-    // Se inserta el nodo en el subárbol derecho
+
     else if (tiempo_total > nodo->tiempo_total) {
         nodo->derecha = insertar(nodo->derecha, nombre, numero, vueltas, tiempo_total);
     }
 
-    // Si los tiempos son iguales, se inserta el nodo en el subárbol izquierdo
+
     else {
         nodo->izquierda = insertar(nodo->izquierda, nombre, numero, vueltas, tiempo_total);
     }
 
-    // Se actualiza la altura del nodo
+
     nodo->altura = 1 + maximo(altura(nodo->izquierda), altura(nodo->derecha));
 
-    // Se verifica si el árbol está balanceado
+
     int factor_balance = balance(nodo);
 
-    // Si el árbol está desbalanceado, se realizan las rotaciones necesarias
+
     if (factor_balance > 1 && tiempo_total < nodo->izquierda->tiempo_total) {
         return rotarDerecha(nodo);
     }
@@ -119,11 +119,11 @@ Nodo* insertar(Nodo* nodo, string nombre, int numero, int vueltas, double tiempo
         return rotarIzquierda(nodo);
     }
 
-    // Se retorna el nodo
+
     return nodo;
 }
 
-// Función para recorrer el árbol en orden y mostrar los tiempos de menor a mayor
+
 void recorrerArbol(Nodo* nodo) {
     if (nodo != NULL) {
         recorrerArbol(nodo->izquierda);
@@ -137,31 +137,51 @@ void recorrerArbol(Nodo* nodo) {
 int main() {
     Nodo* raiz = NULL;
 
-    // Se solicita el ingreso de los datos del atleta
+
     string nombre;
     int numero;
     int vueltas;
     double tiempo_total;
     char respuesta;
+    int op;
 
     do {
-        cout << "Ingrese el nombre del atleta: ";
-        cin >> nombre;
-        cout << "Ingrese el numero del atleta: ";
-        cin >> numero;
-        cout << "Ingrese el numero de vueltas del atleta: ";
-        cin >> vueltas;
-        cout << "Ingrese el tiempo total del atleta: ";
-        cin >> tiempo_total;
+        cout<<"1. REGISTRAR UN ATLETA"<<endl;
+        cin>>op;
+        switch(op){
+            case 1: 
+            
+                    do{
 
-        // Se inserta el nodo en el árbol AVL
-        raiz = insertar(raiz, nombre, numero, vueltas, tiempo_total);
+                    cout<<"Registrar un atleta"<<endl;
 
-        cout << "¿Desea ingresar otro atleta? (S/N): ";
-        cin >> respuesta;
-    } while (respuesta == 'S' || respuesta == 's');
 
-    // Se recorre el árbol y se muestran los tiempos de menor a mayor
+                    cout << "Ingrese el nombre del atleta: ";
+                    cin >> nombre;
+                    cout << "Ingrese el numero del atleta: ";
+                    cin >> numero;
+                    cout << "Ingrese el numero de vueltas del atleta: ";
+                    cin >> vueltas;
+                    cout << "Ingrese el tiempo total del atleta: ";
+                    cin >> tiempo_total;
+
+
+                    raiz = insertar(raiz, nombre, numero, vueltas, tiempo_total);
+
+                    cout << "¿Desea ingresar otro atleta? (S/N): ";
+                    cin >> respuesta;
+
+
+                    } while (respuesta == 'S' || respuesta == 's');
+                    
+
+        }
+
+
+        
+    } while (op<2);
+
+
     recorrerArbol(raiz);
 
     return 0;
